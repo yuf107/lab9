1a1dc64bca18c9d83c0fc3b629390d2a647b800e
Yu Feng <yuf107@ucsd.edu>
2018-03-08 10:13:46 -0800

    Finish lab 9

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
 
 // Function that is called when the document is ready.
 function initializePage() {
-    $.get('/lab/today', onServerResponse);
+    $.get('/lab/today', onServerResponse());
 }
 
 // Function that is called when the server responds to the AJAX call
-function onServerResponse(data){
-    var labElementId = getLabElementId(data.id);
+function onServerResponse(){
+    var labElementId = getLabElementId('9');
     var labElement = $(labElementId);
     labElement.css({'background': 'yellow'});
 }
@@ -20,6 +20,6 @@ function onServerResponse(data){
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
